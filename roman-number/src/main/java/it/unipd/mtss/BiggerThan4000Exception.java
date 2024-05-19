bd8a1cfeada89b71b8036726bd0e51c6886d01b8
Teodora <163033040+LaTeodora@users.noreply.github.com>
2024-05-19 18:23:58 +0200

    close #8

--- roman-number/src/main/java/it/unipd/mtss/BiggerThan4000Exception.java
+++ roman-number/src/main/java/it/unipd/mtss/BiggerThan4000Exception.java
@@ -6,4 +6,7 @@ package it.unipd.mtss;
 
 public class BiggerThan4000Exception extends Exception
 {
+    public BiggerThan4000Exception(){
+        super("Numbers greater than 4000 are not supported!");
+    }
 }
--- roman-number/src/main/java/it/unipd/mtss/ZeroException.java
+++ roman-number/src/main/java/it/unipd/mtss/ZeroException.java
@@ -6,4 +6,7 @@ package it.unipd.mtss;
 
 public class ZeroException extends Exception
 {
+    public ZeroException(){
+        super("Zero doesn't exist in Roman numerlas !");
+    }
 }
