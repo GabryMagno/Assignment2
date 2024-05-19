7d619c17c41ba83808551eb2da23e9546c2e02e1
Teodora <163033040+LaTeodora@users.noreply.github.com>
2024-05-19 17:52:23 +0200

    close #5

---
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////
+//  Gabriele Isacco Magnelli 2075542
+//  Teodora Mihajlovic 2068221
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class IntegerToRoman
+{
+    public static String convert(int number) throws ZeroException,NegativeNumberException,BiggerThan4000Exception{}
+}
